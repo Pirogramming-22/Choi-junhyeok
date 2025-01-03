882e86e2ef7bc0ea255deb59a1ae873f9ac5bd10
junhkchoi <leisure1566@gmail.com>
2025-01-03 13:59:56 +0900

    brGame함수 추가

--- python_problem/homework.py
+++ python_problem/homework.py
@@ -1,30 +1,33 @@
 import random
 
-num = 0
-
-while num < 31:
-    for _ in range(random.choice([1, 2, 3])):
+def brGame(turn, count, num):
+    for _ in range(count):
         num += 1
         if num > 31:
             break
-        print(f"computer {num}")
+        print(f"{turn} {num}")
+    return num
+
+num = 0
+
+while num < 31:
+
+    num = brGame("computer", random.choice([1, 2, 3]), num)
     if num >= 31:
-        print("computer 패배!")
+        print("player win!")
         break
 
-    player_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-    if player_num.isdigit():
-        player_num = int(player_num)
-        if player_num in [1, 2, 3]:
-            for _ in range(player_num):
-                num += 1
-                if num > 31:
-                    break
-                print(f"player {num}")
-            if num >= 31:
-                print("player 패배!")
-                break
+    while True:
+        player_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+        if player_num.isdigit(): 
+            player_num = int(player_num)
+            if player_num in [1, 2, 3]: 
+                num = brGame("player", player_num, num)
+                if num >= 31:
+                    print("computer win!")
+                break  
+            else:
+                print("1, 2, 3 중 하나를 입력하세요.")
         else:
-            print("1, 2, 3 중 하나를 입력하세요.")
-    else:
-        print("정수를 입력하세요.")
+            print("정수를 입력하세요.")
