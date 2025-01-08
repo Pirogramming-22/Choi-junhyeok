577ad73ee8d7ec58345df31fb711fc4f127d9f43
junhkchoi <leisure1566@gmail.com>
2025-01-08 17:01:26 +0900

    설치하기

---
+++ djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/choijunhyeok/Desktop/Piro22 실습/Choi-junhyeok/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
